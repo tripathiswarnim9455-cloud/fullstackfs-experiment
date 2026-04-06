eb753fd9d9380cd16f24c226f7c47fbbac99a76d
tripathiswarnim9455-cloud <tripathiswarnim9455@gmail.com>
2026-04-06 22:38:07 +0530

    Add files via upload

---
+++ Server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/products", require("./routes/productRoutes"));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
